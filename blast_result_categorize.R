e232ec48150a3e0a412cbbdbe4c55c637ec6cde9
neil2003tw <neil2003tw@gmail.com>
2015-05-10 14:44:22 +0800

    Whole new version, grepl for align list fucked up

--- blast_result_categorize.R
+++ blast_result_categorize.R
@@ -1,106 +1,143 @@
-library(tm)
 
-###word construct
-useless_words<-c('clone','sequence','dna','component','noncoding','long','rna','tpa','gpa','bac','protein','predicted','synthetic','construct','mitochondrion','transcrip','cdna','isolate')
 
-###Begin
-result_nr<-readLines(file('data/nr_clean_result_20v0.5kr.out'))
-query<-grepl(']$',result_nr)|grepl('^Query',result_nr)|grepl('[1-9]:[1-9]',result_nr)
-state=0
-score=0
-count=0
-organized_data<-data.frame()
-current_data<-data.frame()
-for(i in 1:length(query)){
-  ###Progress
-  print(paste0((i*100)/length(query),'%'))
+blast_result_categorize<-function(data_path,output_path){
+
+###Make_data_frame
+Main_data<-data.frame()
+Current_data<-data.frame()
+
+###Index preparation
+file_result<-file(data_path)
+blast_result<-readLines(file_result)
+close(file_result)
+index_Cstart<-grepl('^Query=',blast_result)
+index_Length<-grepl('Length=',blast_result)
+index_Glist<-grepl('\\d+\\s+(\\d+e-\\d+)\\s*$', blast_result,perl = T)
+
+###Subgroup data for 1st loop
+index_Step1<-which(index_Cstart|index_Length|index_Glist)
+state_Length<-'Unknown'
+state_list<-'Unrecord'
+
+print('Step 1')
+###Progress bar
+pb <- txtProgressBar(min = 0, max = length(index_Step1), style = 3)
+
+### Step1. Generalized Information
+for(i in 1:length(index_Step1)){
+  ### Progress report
+  setTxtProgressBar(pb, i)
   
-  if(xor(state,query[i])){
-    if(!state){
-      
-      ###Save text mining result
-      if(i != 1){
-        text_tokenize<-table(factor(text_tokenize))
-        if(max(text_tokenize)==min(text_tokenize)){
-          current_data$species<-Reduce(function(x,y) paste(x,y,sep = ' '),names(text_tokenize))
-        }else if(length(text_tokenize)==0){
-          current_data$species<-NA
-        }else if(max(text_tokenize)==2){
-          current_data$species<-Reduce(function(x,y) paste(x,y,sep = ' '),names(text_tokenize[text_tokenize>1]))
-        }else{
-          current_data$species<-Reduce(function(x,y) paste(x,y,sep = ' '),names(text_tokenize[text_tokenize>2]))
-        }
-      }
-      ### Bind data
-      organized_data<-rbind(organized_data,current_data)
-      ### Create new data
-      current_data<-data.frame(name=unname(strsplit(result_nr[i],'[\\ =]')[[1]][3]))
-      current_data$len<-unname(strsplit(result_nr[i],'[\\ =]')[[1]][5])
-    }else if(state){
-      text_tokenize<-c()
-      score=0
-      count=0
-    }
-    state=!state
-  }else if(!state){
-    ##if it is blast mapping line
-    ## Older for proto blast format'[a-z]+\\|[A-Z]+'
-    if(grepl('\\d+\\s+(\\d+\\.\\d+|\\d+e-?\\d+)\\s*$', result_nr[i],perl = T) & count<4){
-      score_grid<-strsplit(result_nr[i],'[ ]+')[[1]]
-      ### if its score is highest and is numeric
-      if( as.numeric(score_grid[length(score_grid)-1])>=score){ #| !is.numeric(score_grid[length(score_grid)-1])){
-        print(paste(score,' ',i))
-      ## if it belongs to older blast format
-        if(grepl('\\|',result_nr[i])){
-          a<-strsplit(result_nr[i],'\\| ')[[1]][2]
-          a<-stripWhitespace(a)
-          a<-removeNumbers(a)
-          a<-removePunctuation(a)
-          a<-tolower(a)
-          a<-removeWords(a,stopwords('english'))
-          a<-removeWords(a,useless_words)
-          a<-strsplit(a,split = ' ')[[1]]
-          a<-a[nchar(a)>2]
-          text_tokenize<-c(text_tokenize,a)
-          count=count+1
-          score=as.numeric(score_grid[length(score_grid)-1])
-        }else{
-          ## Rseudo Reference
-          a<-strsplit(result_nr[i],'  ')[[1]][2]
-          text_tokenize<-c(text_tokenize,a)
-          count=count+1
-        }
-      }
-    }
+  ### Current line mark
+  lmark_decider<-c(New_start=index_Cstart[index_Step1[i]],
+                   Length=index_Length[index_Step1[i]],
+                   Align_list=index_Glist[index_Step1[i]])
+  lmark<-names(which(lmark_decider))
+  #print(lmark)
+  
+  switch(lmark,
+         New_start={
+           ### Arrange previous data
+           if(state_list=='Record'){
+             Current_data<-data.frame(contig_name,contig_length,contig_score,contig_simplspecies)}
+           ### Joint prvious data
+           Main_data<-rbind(Main_data,Current_data)
+           Current_data<-data.frame()
+           contig_name<-strsplit(blast_result[index_Step1[i]],'[\\ =]')[[1]][3]
+           ### State define
+           state_Length<-'Unknown'
+           state_list<-'Unrecord'
+         },
+         Length={
+           if(state_Length=='Unknown'){
+             contig_length<-strsplit(blast_result[index_Step1[i]],'[\\ =]')[[1]][2]
+             state_Length<-'Known'}
+         },
+         Align_list={
+           switch(state_list,
+                  Unrecord={
+                    vector_list<-strsplit(blast_result[index_Step1[i]],'[ ]+')[[1]]
+                    contig_score<-as.numeric(vector_list[length(vector_list)-1])
+                    contig_simplspecies<-paste(vector_list[1:(length(vector_list)-2)],collapse = ' ')
+                    state_list<-'Record'
+                  },
+                  Record={
+                    vector_list<-strsplit(blast_result[index_Step1[i]],'[ ]+')[[1]]
+                    current_score<-as.numeric(vector_list[length(vector_list)-1])
+                    if(current_score==contig_score){
+                      contig_simplspecies<-c(contig_simplspecies,
+                                             paste(vector_list[1:(length(vector_list)-2)],collapse = ' '))}
+                  })
+           
+         })
+}
+
+
+### Step 2. Filled up information
+
+Main_data$full_name<-NA
+Main_data$transcript_length<-NA
+list_contigs<-unname(sapply(blast_result[index_Cstart],
+                            function(x) strsplit(x,'[\\ =]')[[1]][3]))
+table_contig_index<-data.frame(row.names = list_contigs,index=which(index_Cstart))
+index_Detail<-grepl('^>',blast_result)
+index_Step2<-which(index_Cstart|index_Length|index_Detail)
+
+###Progress bar
+pb <- txtProgressBar(min = 0, max = dim(Main_data)[1], style = 3)
+print('Step 2')
+current_contig<-'NONE'
+for(i in 1:dim(Main_data)[1]){
+  setTxtProgressBar(pb, i)
+  if(Main_data[i,1]!=current_contig){
+    current_contig<-as.character(Main_data[i,1])
+    current_index<-which(index_Step2==table_contig_index[current_contig,])
+    Main_data[i,'full_name']<-paste(blast_result[index_Step2[current_index+2]:
+                                                (index_Step2[current_index+3]-1)],collapse =' ')
+    Main_data[i,'transcript_length']<-strsplit(blast_result[index_Step2[current_index+3]],'[\\ =]')[[1]][2]    
+    current_index<-current_index+4
+  }else{
+    Main_data[i,'full_name']<-paste(blast_result[index_Step2[current_index]: 
+                                                   (index_Step2[current_index+1]-1)],collapse = ' ')
+    Main_data[i,'transcript_length']<-strsplit(blast_result[index_Step2[current_index+1]],'[\\ =]')[[1]][2]    
+    current_index<-current_index+2
   }
 }
 
 
-text_tokenize<-table(factor(text_tokenize))
-if(max(text_tokenize)==min(text_tokenize)){
-  current_data$species<-Reduce(function(x,y) paste(x,y,sep = ' '),names(text_tokenize))
-}else if(length(text_tokenize)==0){
-  current_data$species<-NA
-}else if(max(text_tokenize)==2){
-  current_data$species<-Reduce(function(x,y) paste(x,y,sep = ' '),names(text_tokenize[text_tokenize>1]))
-}else{
-  current_data$species<-Reduce(function(x,y) paste(x,y,sep = ' '),names(text_tokenize[text_tokenize>2]))
+
+write.table(Main_data,output_path,quote = F,col.names = T,row.names = F,sep = '\t')
+
 }
-organized_data<-rbind(organized_data,current_data)
 
-#rm(list = ls()[ls()!='organized_data'])
+### Loop start
+output_path_header<-'./Blast_result/'
+if (!file.exists(output_path_header)){
+  dir.create(output_path_header)
+}
 
-RSEM_original<-read.table('data/RSEM.genes.results',header=T)
-organized_data$name<-as.character(organized_data$name)
-organized_data$gene_id<-sapply(organized_data$name,function(x) strsplit(x,split = '_seq')[[1]][1])
-RSEM_original$species_name<-organized_data$species[ match(RSEM_original$gene_id,organized_data$gene_id) ]
-RSEM_original<-RSEM_original[order(RSEM_original$TPM,decreasing = T),]
+file_all<-list.files('./',recursive=T)
+file_blastresult<-grep('nr_result.out$',file_all,value=T)
 
-organized_data$len<-as.numeric(organized_data$len)
-organized_data<-organized_data[order(organized_data$len,decreasing = T),]
-temp<-organized_data[!(grepl('homo',organized_data$species) | grepl('human',organized_data$species)),]
+for(i in 1:length(file_blastresult)){
+  temp<-strsplit(file_blastresult[i],'/')[[1]]
+  output_name<-print(grep('SRR',temp,value=T))
+  print(paste('Start working on',output_name))
+  blast_result_categorize(file_blastresult[i],paste0(output_path_header,output_name,'_out.txt'))
+}
 
 
-temp$True_positive<-grepl('Pseudo',temp$species)
-accuracy<-(sum(temp$True_positive[temp$len>583])+sum(!temp$True_positive[temp$len<583]))/length(temp$len)
-ggplot(temp)+geom_dotplot(aes(x=len,colour=True_positive))+ggtitle('300 Virus reads')+geom_vline(x=583)+xlab('Contigs length')
+#RSEM_original<-read.table('data/RSEM.genes.results',header=T)
+#Main_data$name<-as.character(Main_data$name)
+#Main_data$gene_id<-sapply(Main_data$name,function(x) strsplit(x,split = '_seq')[[1]][1])
+#RSEM_original$species_name<-Main_data$species[ match(RSEM_original$gene_id,Main_data$gene_id) ]
+#RSEM_original<-RSEM_original[order(RSEM_original$TPM,decreasing = T),]
+
+#Main_data$len<-as.numeric(Main_data$len)
+#Main_data<-Main_data[order(Main_data$len,decreasing = T),]
+#temp<-Main_data[!(grepl('homo',Main_data$species) | grepl('human',Main_data$species)),]
+
+
+#temp$True_positive<-grepl('Pseudo',temp$species)
+#accuracy<-(sum(temp$True_positive[temp$len>583])+sum(!temp$True_positive[temp$len<583]))/length(temp$len)
+#ggplot(temp)+geom_dotplot(aes(x=len,colour=True_positive))+ggtitle('300 Virus reads')+geom_vline(x=583)+xlab('Contigs length')
